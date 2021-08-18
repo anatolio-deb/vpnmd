5026c298c52c8d9c8e9d709f56d81d894d80d636
Anatolio <nikiforova693@gmail.com>
2021-08-18 06:42:39 +0500

    refactor: start

--- vpnmd/appd.py
+++ vpnmd/appd.py
@@ -40,55 +40,30 @@ def _iptables_rule_exists(port: str):
 parser = argparse.ArgumentParser()
 parser.add_argument("--version", action="store_true", default=False)
 args = parser.parse_args()
+
 if args.version:
     print(f"vpnmd, v{__version__}")
-
-address = ("localhost", 6554)
-
-try:
-    appd = Appd(address)
-except OSError:
-    pass
 else:
+    address = ("localhost", 6554)
 
-    @appd.api
-    def iptables_rule_exists(port: str) -> bool:
-        return _iptables_rule_exists(port)
+    try:
+        appd = Appd(address)
+    except OSError as exc:
+        print(exc)
+    else:
 
-    @appd.api
-    def delete_dns_rule(port: str) -> subprocess.CompletedProcess:
-        proc = subprocess.run(
-            [
-                "iptables",
-                "-t",
-                "nat",
-                "-D",
-                "OUTPUT",
-                "-p",
-                "udp",
-                "--dport",
-                "53",
-                "-j",
-                "REDIRECT",
-                "--to-ports",
-                port,
-            ],
-            check=False,
-            capture_output=True,
-        )
-        return proc
+        @appd.api
+        def iptables_rule_exists(port: str) -> bool:
+            return _iptables_rule_exists(port)
 
-    @appd.api
-    def add_dns_rule(port: str) -> subprocess.CompletedProcess | None:
-        proc = None
-
-        if not _iptables_rule_exists(port):
+        @appd.api
+        def delete_dns_rule(port: str) -> subprocess.CompletedProcess:
             proc = subprocess.run(
                 [
                     "iptables",
                     "-t",
                     "nat",
-                    "-A",
+                    "-D",
                     "OUTPUT",
                     "-p",
                     "udp",
@@ -102,109 +77,134 @@ else:
                 check=False,
                 capture_output=True,
             )
-        return proc
+            return proc
 
-    @appd.api
-    def delete_iface(ifindex: int) -> subprocess.CompletedProcess:
-        proc = subprocess.run(
-            ["ip", "tuntap", "del", "dev", f"tun{ifindex}", "mode", "tun"],
-            check=False,
-            capture_output=True,
-        )
-        return proc
+        @appd.api
+        def add_dns_rule(port: str) -> subprocess.CompletedProcess | None:
+            proc = None
 
-    @appd.api
-    def add_iface(ifindex: int, ifaddr: str) -> subprocess.CompletedProcess:
-        try:
+            if not _iptables_rule_exists(port):
+                proc = subprocess.run(
+                    [
+                        "iptables",
+                        "-t",
+                        "nat",
+                        "-A",
+                        "OUTPUT",
+                        "-p",
+                        "udp",
+                        "--dport",
+                        "53",
+                        "-j",
+                        "REDIRECT",
+                        "--to-ports",
+                        port,
+                    ],
+                    check=False,
+                    capture_output=True,
+                )
+            return proc
+
+        @appd.api
+        def delete_iface(ifindex: int) -> subprocess.CompletedProcess:
             proc = subprocess.run(
-                ["ip", "tuntap", "add", "dev", f"tun{ifindex}", "mode", "tun"],
-                check=True,
-                capture_output=True,
-            )
-        except subprocess.CalledProcessError as ex:
-            logging.exception(ex)
-        else:
-            proc = subprocess.run(
-                ["ip", "address", "add", f"{ifaddr}", "dev", f"tun{ifindex}"],
+                ["ip", "tuntap", "del", "dev", f"tun{ifindex}", "mode", "tun"],
                 check=False,
                 capture_output=True,
             )
-        return proc
+            return proc
 
-    @appd.api
-    def set_iface_up(ifindex: int) -> subprocess.CompletedProcess:
-        """tun2socks must be running"""
-        proc = subprocess.run(
-            ["ip", "link", "set", "dev", f"tun{ifindex}", "up"],
-            check=False,
-            capture_output=True,
-        )
-        return proc
+        @appd.api
+        def add_iface(ifindex: int, ifaddr: str) -> subprocess.CompletedProcess:
+            try:
+                proc = subprocess.run(
+                    ["ip", "tuntap", "add", "dev", f"tun{ifindex}", "mode", "tun"],
+                    check=True,
+                    capture_output=True,
+                )
+            except subprocess.CalledProcessError as ex:
+                logging.exception(ex)
+            else:
+                proc = subprocess.run(
+                    ["ip", "address", "add", f"{ifaddr}", "dev", f"tun{ifindex}"],
+                    check=False,
+                    capture_output=True,
+                )
+            return proc
 
-    @appd.api
-    def add_default_route(metric: int, ifindex: int) -> subprocess.CompletedProcess:
-        """Device must be UP"""
-        proc = subprocess.run(
-            [
-                "ip",
-                "route",
-                "add",
-                "default",
-                "dev",
-                f"tun{ifindex}",
-                "metric",
-                f"{metric}",
-            ],
-            check=False,
-            capture_output=True,
-        )
-        return proc
+        @appd.api
+        def set_iface_up(ifindex: int) -> subprocess.CompletedProcess:
+            """tun2socks must be running"""
+            proc = subprocess.run(
+                ["ip", "link", "set", "dev", f"tun{ifindex}", "up"],
+                check=False,
+                capture_output=True,
+            )
+            return proc
 
-    @appd.api
-    def delete_node_route(
-        node_address: str, original_gateway: str
-    ) -> subprocess.CompletedProcess:
-        proc = subprocess.run(
-            [
-                "ip",
-                "route",
-                "delete",
-                f"{node_address}",
-                "via",
-                f"{original_gateway}",
-            ],
-            check=False,
-            capture_output=True,
-        )
-        return proc
+        @appd.api
+        def add_default_route(metric: int, ifindex: int) -> subprocess.CompletedProcess:
+            """Device must be UP"""
+            proc = subprocess.run(
+                [
+                    "ip",
+                    "route",
+                    "add",
+                    "default",
+                    "dev",
+                    f"tun{ifindex}",
+                    "metric",
+                    f"{metric}",
+                ],
+                check=False,
+                capture_output=True,
+            )
+            return proc
 
-    @appd.api
-    def add_node_route(
-        node_address: str, original_gateway: str, metric: int
-    ) -> subprocess.CompletedProcess:
-        """Add the route for the v2ray node through the original gateway
+        @appd.api
+        def delete_node_route(
+            node_address: str, original_gateway: str
+        ) -> subprocess.CompletedProcess:
+            proc = subprocess.run(
+                [
+                    "ip",
+                    "route",
+                    "delete",
+                    f"{node_address}",
+                    "via",
+                    f"{original_gateway}",
+                ],
+                check=False,
+                capture_output=True,
+            )
+            return proc
 
-        Args:
-            node_address (str): An IP address of the v2ray node
-            original_gateway (str): The original gateway of the host
-            metric (int): The route metric should be lower than the one \
-                of the original default route
-        """
-        proc = subprocess.run(
-            [
-                "ip",
-                "route",
-                "add",
-                f"{node_address}",
-                "via",
-                f"{original_gateway}",
-                "metric",
-                f"{metric}",
-            ],
-            check=False,
-            capture_output=True,
-        )
-        return proc
+        @appd.api
+        def add_node_route(
+            node_address: str, original_gateway: str, metric: int
+        ) -> subprocess.CompletedProcess:
+            """Add the route for the v2ray node through the original gateway
 
-    if not args.version:
+            Args:
+                node_address (str): An IP address of the v2ray node
+                original_gateway (str): The original gateway of the host
+                metric (int): The route metric should be lower than the one \
+                    of the original default route
+            """
+            proc = subprocess.run(
+                [
+                    "ip",
+                    "route",
+                    "add",
+                    f"{node_address}",
+                    "via",
+                    f"{original_gateway}",
+                    "metric",
+                    f"{metric}",
+                ],
+                check=False,
+                capture_output=True,
+            )
+            return proc
+
         appd.start()
